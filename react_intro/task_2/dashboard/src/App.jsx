a0c9a323cb44ca784b86910c92ef41071273c1a3
silviazylali <silviazylali0707@gmail.com>
2025-09-18 17:49:35 +0200

    Task 3

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
 import holbertonLogo from './assets/holberton-logo.jpg'
-import { getCurrentYear, getFooterCopy } from './utils'
+import { getCurrentYear, getFooterCopy } from './utils.js'
 import Notifications from './Notifications.jsx'
-
-
-
-
 
 function App() {
   return (
     <>
-    <div>
-      <Notifications />
-    </div>
+      <div>
+        <Notifications />
+      </div>
       <div className="App-header">
         <img src={holbertonLogo} alt='holberton logo' />
         <h1 style={{ color: '#E1003C' }} >School dashboard</h1>
@@ -20,10 +16,10 @@ function App() {
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
         <label htmlFor="email">email</label>
-        <input type='email' id='email' >email</input>
+        <input type='email' id='email'></input>
 
-        <label htmlFor='password'>password</label>
-        <input type='password' id='password' >password</input>
+        <label htmlFor='password'></label>
+        <input type='password' id='password'></input>
         <button type='submit'>OK</button>
       </div>
       <div className='App-footer'>
--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -2,7 +2,7 @@ import './Notifications.css'
 import { getLatestNotification } from './utils'
 import closeIcon from './assets/close-button.png'
 
-export default function Notifications() {
+function Notifications() {
 
   const handleClick = () => {
     console.log("Close button has been clicked");
@@ -15,8 +15,7 @@ export default function Notifications() {
       <ul>
         <li data-priority="default">New course available</li>
         <li data-priority="urgent">New resume available</li>
-        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}>
-        </li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
       </ul>
       <button style={{ position: "absolute", top: "15px", right: "20px" }} aria-label="Close" onClick={handleClick}>
         <img style={{ width: "10px", height: "10px" }} src={closeIcon} />
@@ -24,3 +23,4 @@ export default function Notifications() {
     </div>
   )
 }
+export default Notifications;
