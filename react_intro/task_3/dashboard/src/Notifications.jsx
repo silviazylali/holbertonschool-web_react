2f9fca10ebdf06560ebc59b29065619fd8ad7237
silviazylali <silviazylali0707@gmail.com>
2025-09-18 19:01:42 +0200

    Package.json

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import "./Notifications.css";
+import closeIcon from "./assets/close-button.png";
+import { getLatestNotification } from "./utils";
 
-export default function Notifications() {
+function Notifications() {
+
     const handleClick = () => {
         console.log("Close button has been clicked");
-    };
+    }
 
     return (
-        <div className="Notifications">
-            <button aria-label="Close" onClick={handleClick}>Close</button>
+        <div className="notification-items">
             <p>Here is the list of notifications</p>
             <ul>
-                <li>New course available</li>
-                <li>New resume available</li>
-                <li>New data available</li>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}>
+                </li>
             </ul>
-        </div>
-    );
+            <button style={{ position: "absolute", top: "15px", right: "20px" }} aria-label="Close" onClick={handleClick}>
+                <img style={{ width: "10px", height: "10px" }} src={closeIcon} />
+            </button>
+        </div >
+    )
 }
+
+export default Notifications;
