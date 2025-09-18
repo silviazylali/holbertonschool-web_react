ee12639c0a8de3acdc006beb760eb3e999469e08
silviazylali <silviazylali0707@gmail.com>
2025-09-18 18:09:24 +0200

    app.spec.js

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -15,10 +15,10 @@ function App() {
       </div>
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
-        <label htmlFor="email"></label>
+        <label htmlFor="email">email</label>
         <input type="email" id="email"></input>
 
-        <label htmlFor="password"></label>
+        <label htmlFor="password">password</label>
         <input type="password" id="password"></input>
 
         <button type="submit">OK</button>
